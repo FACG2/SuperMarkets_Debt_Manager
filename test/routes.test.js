4319b1da45580121d47e11c3f9ab9f48b433ad7b
EngAyoosh <aaa_2008azhar@hotmail.com>
2017-10-01 17:14:20 +0300

    adding more tests for routes

--- test/routes.test.js
+++ test/routes.test.js
@@ -4,7 +4,6 @@ const app = require('../app/app.js');
 
 const test = require('tape');
 const token = process.env.EXAMTOKEN;
-console.log(token);
 // request(app)
 //   .get('/')
 //   .expect('Content-Type', 'text/plain; charset=utf-8')
@@ -19,27 +18,57 @@ test('GET /login', function (assert) {
       .expect(200)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .end(function (err, res) {
-        var expectedThings = {};
-        var actualThings = res.body;
-
-        assert.error(err, 'No error');
-        assert.same(actualThings, expectedThings, 'Retrieve list of things');
+        if (err) throw err;
         assert.end();
+        // var expectedThings = {};
+        // var actualThings = res.body;
+        //
+        // assert.error(err, 'No error');
+        // assert.same(actualThings, expectedThings, 'Retrieve list of things');
+        // assert.end();
       });
 });
 
 test('POST /login', function (assert) {
-  var newThing = { email: 'aaa_2008azhar@hotmail.com', password: '44567'};
+  var newUser = { email: 'aaa_2008azhar@hotmail.com', password: '44567'};
   request(app)
     .post('/login')
-    .send(newThing)
+    .send(newUser)
     .expect(302)
     .expect('Content-Type', 'text/plain; charset=utf-8')
     .end(function (err, res) {
-      var actualThing = res.body;
-
-      assert.error(err, 'No error');
-      assert.same(actualThing, {}, 'Create a new thing');
+      assert.error(err, 'error');
+      assert.equal(res.statusCode, 302, 'Status Code 302');// statuscode === 302
+      assert.equal(res.headers.location, '/', 'redirect to homepage');// res.headers.location === '/'
+    //  console.log(res.headers['set-cookie'][0].split('token=')[1].split(';')[0]);
+      // assert.equal(res.headers['set-cookie'][0].split('token=')[1].split(';')[0], token, 'check the cookie');// res.cookie
       assert.end();
     });
+});
+
+test('Testing the the home page if the reqest have an authinticated token ', (t) => {
+  request(app)
+  .get('/')
+  .set('Cookie', 'token=' + token)
+  .expect(200)
+  .expect('Content-Type', 'text/html; charset=utf-8')
+  .end((err, res) => {
+    t.error(err, 'error');
+    t.equal(res.statusCode, 200, 'Status Code 200');
+    t.end();
+  });
+});
+test.only('Testing the the home page if the reqest have an unauthinticated token ', (t) => {
+  request(app)
+  .get('/')
+  .set('Cookie', 'token=77' + token)
+  .expect(500)
+  .expect('Content-Type', 'text/html; charset=utf-8')
+  .end((err, res) => {
+    t.error(err, 'error');
+    t.equal(res.headers['x-powered-by'], undefined, 'Check the framework woh power the server');
+    t.equal(res.statusCode, 500, 'Status Code 500');
+    t.equal(res.text, 'Something broke! - Server Error 500', 'Should respons with error message');
+    t.end();
+  });
 });
