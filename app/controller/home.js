2ecdd5e425b4bca365835c27576ea89c5ff46edc
eslam3bed <iah-93@hotmail.com>
2017-10-03 09:39:06 +0300

    refactoring the search routes

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,9 +1,9 @@
-const {getAllDebts} = require('../models/queries/db_queries.js');
+const {getAllDebts} = require('../models/queries/db_queries.js')
 module.exports = (req, res, next) => {
   // modify this code (just example)
-  const { username, userID } = req.user;
+  const { username, userID } = req.user
   getAllDebts(userID, (err, result) => {
-    if (err) return next(err);
-    res.render('home', {username: username, total: result.rows[0].sum});
-  });
-};
+    if (err) return next(err)
+    res.render('home', {username: username, total: result.rows[0].sum})
+  })
+}
